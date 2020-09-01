78f3b778d27dfe897979f660ca1b5e62596440b4
yusluv1234 <yusluv124@gmail.com>
2020-09-01 22:24:04 +0100

    adding Home and about

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     static defaultProps = {
@@ -17,7 +18,15 @@ class Navbar extends Component {
             <nav className="navbar bg-primary">
                 <h1>
                     <i className={this.props.icon} /> {this.props.title}
-                    </h1>
+                </h1>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/About'>About</Link>
+                    </li>
+                </ul>
             </nav>
         )
     }
