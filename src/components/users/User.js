6170cfcba3d0d6b95607454a4c73b83298fdc8c8
yusluv1234 <yusluv124@gmail.com>
2020-09-05 23:05:34 +0100

    add some users content

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
 
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
+    }
+
     render() {
         const {
             name,
+            company, 
             avatar_url,
             location,
             bio,
@@ -24,9 +34,18 @@ export class User extends Component {
 
         const { loading } = this.props;
 
+        if (loading) return <Spinner />;
+
         return (
-            <div>{name}</div>
-        )
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>
+                hireable: {' '}
+                {hireable ? <i className='fas fa-check text-success' /> :
+                    <i className='fas fa-times-circle text-danger' /> }
+            </Fragment>
+        );
     }
 }
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,13 +4,12 @@ import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
-  console.log(users)
     if (loading) {
       return <Spinner />
     } else {
       return (
         <div style={userStyle}>
-          {users.map=((user) => (
+          {users.map((user) => (
             <UserItem key={user.id} user={user} />
           ))}
         </div>
