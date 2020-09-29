4f267f3e99973c6441f4a666c008f0bcdb1854a9
yusluv1234 <yusluv124@gmail.com>
2020-09-30 00:42:29 +0100

    alertContext

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,18 @@
 import React, { useState, useContext } from 'react';
 import PropTypes from "prop-types";
-import GithubContext from '../../context/github/githubContext';
+import GithubContext from "../../context/github/githubContext";
+import AAAlertContext from "../../context/alert/AlertState";
 
 const Search = ({ setAlert }) => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
 
   const [text, setText] = useState('');
     
     const onSubmit = e => {
         e.preventDefault();
         if (text === '') {
-            setAlert('Please enter something', 'light');
+            alertContext.setAlert('Please enter something', 'light');
         } else {
           githubContext.searchUsers(text);
           setText('');
