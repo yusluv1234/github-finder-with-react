3088903a60da198debe7fd9b8a80ca21f18dbb3a
yusluv1234 <yusluv124@gmail.com>
2020-09-30 00:33:13 +0100

    adding alertContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
