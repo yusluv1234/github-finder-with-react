ac4258193d73b572846b86f136198b4c05e5f76c
yusluv1234 <yusluv124@gmail.com>
2020-09-29 23:39:44 +0100

    adding alertState

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -8,81 +8,22 @@ import {
 } from "../types";
 
 const AlertState = (props) => {
-  const initialState = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-  };
+    const initialState = null;
 
-  const [state, dispatch] = useReducer(GithubReducer, initialState);
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
 
-  // Search users
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id
-      =${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret
-      =${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    dispatch({
-      type: SEARCH_USERS,
-      payload: res.data.items,
-    });
-  };
-
-  // Get user:
-  const getUser = async (username) => {
-    setLoading();
-
-    const res = await axios.get(`https://api.github.com/search/users/${username}?client_id
-      =${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret
-      =${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    dispatch({
-      type: GET_USER,
-      payload: res.data,
-    });
-  };
-
-  // Get Repos
-  const getUserRepos = async (username) => {
-    setLoading();
-
-    const res = await axios.get(
-      `https://api.github.com/search/users/${username}/repos?per_page=5&sort=created:asc&?client_id
-      =${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret
-      =${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    dispatch({
-      type: GET_REPOS,
-      payload: res.data,
-    });
-  };
-
-  // Clear user
-  const clearUsers = () => dispatch({ type: CLEAR_USERS });
-
-  // Set Loading
-  const setLoading = () => dispatch({ type: SET_LOADING });
-
+  // Set  Alert
+    
   return (
-    <GithubContext.Provider
+    <AlertContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+            alert: state,
+            setAlert
       }}
     >
       {props.children}
-    </GithubContext.Provider>
+    </AlertContext.Provider>
   );
 };
 
-export default GithubState;
+export default AlertState;
